b0c03f28bcc2cf27b99cd43dd9d251b911591309
Alejandro Krumkamp <alekd9@gmail.com>
2017-04-13 16:38:15 -0300

    Restaurando archivos eliminados de consola

---
+++ consola/conexiones.h
@@ -0,0 +1,17 @@
+/*
+ * consola.h
+ *
+ *  Created on: 5/4/2017
+ *      Author: utnso
+ */
+
+#ifndef CONEXIONES_H_
+#define CONEXIONES_H_
+
+struct data_socket crearSocket(int, char* );
+int enviarMensaje(int ,char*,int );
+int conectar(int , char* );
+
+
+
+#endif /* CONEXIONES_H_ */
