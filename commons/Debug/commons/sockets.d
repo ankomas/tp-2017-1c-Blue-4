b3c4996634d53110573e8ecd66c5a616ea91956d
ankomas <nicolasmbaltar@gmail.com>
2017-04-13 17:54:59 -0300

    Borrando Debug de todas las carpetas

--- commons/Debug/commons/sockets.d
+++
@@ -1,8 +0,0 @@
-commons/sockets.d: ../commons/sockets.c ../commons/sockets.h \
- ../commons/collections/list.h ../commons/collections/node.h
-
-../commons/sockets.h:
-
-../commons/collections/list.h:
-
-../commons/collections/node.h:
