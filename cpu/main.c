9dbfb0d90db012114ceebcb70fef1822a0cd2883
ankomas <nicolasmbaltar@gmail.com>
2017-04-13 21:36:59 -0300

    CPU se conecta a kernel memoria (falta probar), levanta y muestra config por pantalla

--- cpu/main.c
+++ cpu/main.c
@@ -6,9 +6,27 @@
  */
 #include <stdio.h>
 #include <blue4-lib.h>
+#include <commons/config.h>
+#include "conexiones.h"
 
 int main(){
-	printf("Hello world!\n");
+	int puertoKernel,puertoMemoria,socketKernel,socketMemoria;
+	char *ipKernel,*ipMemoria;
+	t_config* config;
+
+	config=config_create("config.cfg");
+
+	ipKernel=config_get_string_value(config,"IP_KERNEL");
+	ipMemoria=config_get_string_value(config,"IP_MEMORIA");
+	puertoKernel=config_get_int_value(config,"PUERTO_KERNEL");
+	puertoMemoria=config_get_int_value(config,"PUERTO_MEMORIA");
+
+	printf("IP KERNEL: %s\nPUERTO KERNEL: %i\nIP MEMORIA: %s\nPUERTO MEMORIA: %i\n",ipKernel,puertoKernel,ipMemoria,puertoMemoria);
+
+	config_destroy(config);
+
+	//socketKernel=conectar(puertoKernel,ipKernel);
+	//socketMemoria=conectar(puertoMemoria,ipMemoria);
 
 	return 0;
 }
