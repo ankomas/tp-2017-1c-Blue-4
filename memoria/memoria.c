ff759bcf7e7f92dd6034c2053821a8491ff4103a
tfernandez96 <tomi.r.fernandez@gmail.com>
2017-04-27 19:37:03 -0300

    Se añadio el menu de consola en un hilo

--- memoria/memoria.c
+++ memoria/memoria.c
@@ -6,12 +6,15 @@
 #include <blue4-lib.h>
 #include <commons/config.h>
 #include "sockets.h"
+#include "menuMemoria.h"
 
 //esta es mi memoria (Si, Delegue!)
 
 int main(void) {
-
+	pthread_t hiloMostrarMenu;
+	pthread_create(&hiloMostrarMenu, NULL, (void *) mostrarMenuMemoria, NULL);
 	servidor();
+	pthread_join(hiloMostrarMenu, NULL);
 
 	return 0;
 
---
+++ memoria/menuMemoria.h
@@ -0,0 +1,15 @@
+/*
+ * menuMemoria.h
+ *
+ *  Created on: 27/4/2017
+ *      Author: utnso
+ */
+
+#ifndef MENUMEMORIA_H_
+#define MENUMEMORIA_H_
+
+void mostrarMenuMemoria();
+
+int retardoMemoria;
+
+#endif /* MENUMEMORIA_H_ */
