354f9ceee8eaf71de06d199dfd438d8661599c7a
Alejandro Krumkamp <alekd9@gmail.com>
2017-06-27 15:38:32 -0300

    debugging Memoria

---
+++ memoria/Debug/sockets.d
@@ -0,0 +1,16 @@
+sockets.d: ../sockets.c \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/blue4-lib/blue4-lib.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h \
+ ../memoria.h ../operacionesMemoria.h ../sockets.h ../configuraciones.h
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/blue4-lib/blue4-lib.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h:
+
+../memoria.h:
+
+../operacionesMemoria.h:
+
+../sockets.h:
+
+../configuraciones.h:
