d17beaea3bf6fad3d8e4819c6e6a7abef858a3d2
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-14 17:37:34 -0300

    quito /Debugs

--- cpu/primitivasKernel.h
+++ cpu/primitivasKernel.h
@@ -20,6 +20,9 @@ void dummy_liberar(t_puntero puntero);
 t_descriptor_archivo dummy_abrir(t_direccion_archivo direccion, t_banderas flags);
 void dummy_borrar(t_descriptor_archivo descriptor_archivo);
 void dummy_cerrar(t_descriptor_archivo descriptor_archivo);
+void dummy_moverCursor(t_descriptor_archivo descriptor_archivo, t_valor_variable posicion);
+void dummy_escribir(t_descriptor_archivo descriptor_archivo, void* informacion, t_valor_variable tamanio);
+void dummy_leer(t_descriptor_archivo descriptor_archivo, t_puntero informacion, t_valor_variable tamanio);
 
 AnSISOP_kernel kernel_functions;
 
