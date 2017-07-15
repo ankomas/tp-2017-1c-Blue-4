66806252fd8014b80582598996dea4b0321e38ae
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-15 03:41:02 -0300

    agrego crearArchivo a abrirFD

--- kernel/capaFs.c
+++ kernel/capaFs.c
@@ -192,7 +192,7 @@ uint32_t abrirFD(uint32_t i,t_programa* unPrograma){
 		log_error(logger,"Path NULL o permisos NULL");
 		return 9999;
 	}
-	if(validarArchivo(path,strlen(path)) == 1 || !tienePermisos('c',permisos)){
+	if(validarArchivo(path,strlen(path)) == 0 && !tienePermisos('c',permisos)){
 		send(i,"N",1,0);
 		log_error(logger,"No hay permisos para crear un nuevo archivo o el archivo ya esta abierto");
 		return 9999;
@@ -213,6 +213,7 @@ uint32_t abrirFD(uint32_t i,t_programa* unPrograma){
 		nuevaEntradaTGA->indice = GlobalFDCounter;
 		list_add(tablaGlobalArchivos,nuevaEntradaTGA);
 		GlobalFDCounter++;
+		crearArchivo(path,strlen(path));
 		log_trace(logger,"Nuevo FD creado");
 	}
 
