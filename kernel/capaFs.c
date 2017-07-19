9bb5e5488f0a128f5355c04defe1f2818217b087
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-18 21:33:41 -0300

    AbrirFS fixed

--- kernel/capaFs.c
+++ kernel/capaFs.c
@@ -214,12 +214,12 @@ uint32_t abrirFD(uint32_t i,t_programa* unPrograma){
 		log_error(logger,"Path NULL o permisos NULL");
 		return 9999;
 	}
-	if(validarArchivo(path,strlen(path)) && !tienePermisos('c',permisos)){
+	if(!validarArchivo(path,strlen(path)) && !tienePermisos('c',permisos)){
 		send(i,"N",1,0);
-		log_error(logger,"No hay permisos para crear un nuevo archivo o el archivo ya esta abierto");
+		log_error(logger,"El archivo no existe y no hay permisos para crear un nuevo archivo");
 		return 9999;
 	}
-
+	test("A");
 	unPrograma->FDCounter++;
 	t_entradaTAP * nuevaEntradaTAP = malloc(sizeof(nuevaEntradaTAP));
 	nuevaEntradaTAP->flags = permisos;
