2e989b56c5a07499d1676e870eaf4d54898ac477
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-25 03:41:47 -0300

    probando makefiles

---
+++ parser/Debug/parser/parser.d
@@ -0,0 +1,17 @@
+parser/parser.d: ../parser/parser.c ../parser/parser.h ../parser/sintax.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/node.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/error.h
+
+../parser/parser.h:
+
+../parser/sintax.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/node.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/error.h:
