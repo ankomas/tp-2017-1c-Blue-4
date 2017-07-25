5d0dc339d190f70d03a0eb3e2b35fcbc764ba3ed
Alejandro Krumkamp <alekd9@gmail.com>
2017-07-25 03:58:46 -0300

    makefile kernel

---
+++ kernel/Debug/capaFs.d
@@ -0,0 +1,64 @@
+capaFs.d: ../capaFs.c ../capaFs.h ../main.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/config.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/node.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/log.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/blue4-lib/blue4-lib.h \
+ ../capaMemoria.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/queue.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/list.h \
+ ../planificador.h ../pcb.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/list.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/metadata_program.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/parser.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/sintax.h \
+ /home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/error.h \
+ ../consolaKernel.h ../sockets.h ../heap.h ../heapNico.h
+
+../capaFs.h:
+
+../main.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/config.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/node.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/dictionary.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/log.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/string.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/blue4-lib/blue4-lib.h:
+
+../capaMemoria.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/queue.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/list.h:
+
+../planificador.h:
+
+../pcb.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/collections/list.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/metadata_program.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/parser.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/parser/parser/sintax.h:
+
+/home/utnso/workspace/tp-2017-1c-Blue-4/commons/commons/error.h:
+
+../consolaKernel.h:
+
+../sockets.h:
+
+../heap.h:
+
+../heapNico.h:
