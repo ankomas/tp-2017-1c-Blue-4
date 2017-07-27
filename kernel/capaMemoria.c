3ae94cf5c29725c4d7a90bbe8e31c8429ee58b48
Alejandro <alekd9@gmail.com>
2017-07-27 17:45:24 -0300

    Update capaMemoria.c

--- kernel/capaMemoria.c
+++ kernel/capaMemoria.c
@@ -31,15 +31,18 @@ int obtenerTamanioPagina(){
 	char*paquete = malloc(4);
 	int res;
 	if(idUMC > 0){
+		pthread_mutex_lock(&mutex_memoria);
 		send(idUMC,"P",1,0);
 		if(recv(idUMC,paquete,4,MSG_WAITALL) == 4){
 			res=atoi(paquete);
 			free(paquete);
+			pthread_mutex_unlock(&mutex_memoria);
 			return res;
 		}
 	}
 	log_error(logger,"No se pudo obtener el tamanio de pagina");
 	free(paquete);
+	pthread_mutex_unlock(&mutex_memoria);
 	return -1;
 }
 
@@ -102,19 +105,26 @@ int guardarEnMemoria(uint32_t i, uint32_t pid,uint32_t pagina,uint32_t offset,ui
 			);
 	char* streamTamPaquete = intToStream(paquete.data_size);
 
+	pthread_mutex_lock(&mutex_memoria);
 	// Envio de opcode
-	if(sendall(i,"W",&tamOpCode) < 0)
+	if(sendall(i,"W",&tamOpCode) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 
 	// Envio de tamanio de paquete
-	if(sendall(i, streamTamPaquete, &tamInt) < 0)
+	if(sendall(i, streamTamPaquete, &tamInt) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 	free(streamTamPaquete);
 
 	// Envio de paquete
 
-	if(sendall(i, paquete.data, &paquete.data_size) < 0)
+	if(sendall(i, paquete.data, &paquete.data_size) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 	free(paquete.data);
 
 	free(streamPID);
@@ -125,16 +135,20 @@ int guardarEnMemoria(uint32_t i, uint32_t pid,uint32_t pagina,uint32_t offset,ui
 	char* respuesta = malloc(1);
 	if(recv(i,respuesta,1,0) < 1){
 		free(respuesta);
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	} else {
 		if(respuesta[0] == 'N'){
 			free(respuesta);
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		} else {
 			free(respuesta);
+			pthread_mutex_unlock(&mutex_memoria);
 			return 0;
 		}
 	}
+	pthread_mutex_unlock(&mutex_memoria);
 }
 
 int cargarDeMemoria(int socket,uint32_t pid,uint32_t pag, uint32_t off,uint32_t size, package_t* paqueteParametro){
@@ -150,14 +164,17 @@ int cargarDeMemoria(int socket,uint32_t pid,uint32_t pag, uint32_t off,uint32_t 
 			sizeof(uint32_t),&off,
 			sizeof(uint32_t),&size
 			);
-
+	pthread_mutex_lock(&mutex_memoria);
 	if(send(socket,"R",1,0)==-1){
+		pthread_mutex_unlock(&mutex_memoria);
 		perror("Error socket memoria");
 	}
 	if(send(socket,&paquete.data_size,sizeof(uint32_t),0)==-1){
+		pthread_mutex_unlock(&mutex_memoria);
 		perror("Error socket memoria");
 	}
 	if(send(socket,paquete.data,paquete.data_size,0)==-1){
+		pthread_mutex_unlock(&mutex_memoria);
 		perror("Error socket memoria");
 	}
 
@@ -170,6 +187,7 @@ int cargarDeMemoria(int socket,uint32_t pid,uint32_t pag, uint32_t off,uint32_t 
 	if(res[0]=='N'){
 		log_error(logger,"Error al recibir contenido de la memoria\n");
 		free(res);
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	}
 	/*res=realloc(res,sizeof(uint32_t));
@@ -186,7 +204,7 @@ int cargarDeMemoria(int socket,uint32_t pid,uint32_t pag, uint32_t off,uint32_t 
 
 	paqueteParametro->data_size=size;
 	paqueteParametro->data=res;
-
+	pthread_mutex_unlock(&mutex_memoria);
 	return 0;
 }
 
@@ -200,19 +218,25 @@ int inicializarEnMemoria(uint32_t i, uint32_t pid,uint32_t paginasNecesarias) {
 
 	paquete = serializar(4,tamInt, streamPID,tamInt, streamCantPaginas);
 	char* streamTamPaquete = intToStream(paquete.data_size);
-
+	pthread_mutex_lock(&mutex_memoria);
 	// Envio de opcode
-	if(sendall(i,"I",&tamOpCode) < 0)
+	if(sendall(i,"I",&tamOpCode) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 
 	// Envio de tamanio de paquete
-	if(sendall(i, streamTamPaquete, &tamInt) < 0)
+	if(sendall(i, streamTamPaquete, &tamInt) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 	free(streamTamPaquete);
 
 	// Envio de paquete
-	if(sendall(i, paquete.data, &paquete.data_size) < 0)
+	if(sendall(i, paquete.data, &paquete.data_size) < 0){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
+	}
 	free(paquete.data);
 
 	free(streamPID);
@@ -221,22 +245,26 @@ int inicializarEnMemoria(uint32_t i, uint32_t pid,uint32_t paginasNecesarias) {
 	char* respuesta = malloc(1);
 	if(recv(i,respuesta,1,0) < 1){
 		free(respuesta);
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	} else {
 		if(respuesta[0] == 'N'){
 			free(respuesta);
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		} else {
 			free(respuesta);
+			pthread_mutex_unlock(&mutex_memoria);
 			return 0;
 		}
 	}
+	pthread_mutex_unlock(&mutex_memoria);
 }
 
 int pedirPagias(uint32_t pid,uint32_t cantPaginas){
 	package_t paquete;
 	char res;
-
+	pthread_mutex_lock(&mutex_memoria);
 	log_trace(logger,"Llamada a PEDIR PAGINAS");
 	send(idUMC,"A",1,0);
 	paquete=serializar(4,
@@ -252,12 +280,15 @@ int pedirPagias(uint32_t pid,uint32_t cantPaginas){
 	switch(res){
 	case 'Y':
 		log_trace(logger,"Llamada a PEDIR PAGINAS correcto");
+		pthread_mutex_unlock(&mutex_memoria);
 		return 1;
 	case 'N':
 		log_error(logger,"Llamada a PEDIR PAGINAS no se pudo asignar paginas");
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	default:
 		log_error(logger,"Llamada a PEDIR PAGINAS respuesta invalida");
+		pthread_mutex_unlock(&mutex_memoria);
 		return -2;
 	}
 
@@ -266,7 +297,7 @@ int pedirPagias(uint32_t pid,uint32_t cantPaginas){
 int liberarPagina(uint32_t pid,uint32_t pagina){
 	package_t paquete;
 	char res;
-
+	pthread_mutex_lock(&mutex_memoria);
 	send(idUMC,"E",1,0);
 	paquete=serializar(4,
 			sizeof(uint32_t),&pid,
@@ -279,20 +310,25 @@ int liberarPagina(uint32_t pid,uint32_t pagina){
 	switch(res){
 	case 'Y':
 		log_trace(logger,"Llamada a ELIMINAR PAGINA correcto");
+		pthread_mutex_unlock(&mutex_memoria);
 		return 1;
 	case 'N':
 		log_error(logger,"Llamada a ELIMINAR PAGINA no se pudo eliminar pagina");
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	default:
 		log_error(logger,"Llamada a ELIMINAR PAGINA respuesta invalida");
+		pthread_mutex_unlock(&mutex_memoria);
 		return -2;
 	}
 
 }
 
 int finalizarProcesoMemoria(uint32_t pid,bool force){
+	pthread_mutex_lock(&mutex_memoria);
 	t_programa * aux = encontrarProgramaPorPID(pid);
 	if(aux == NULL){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1;
 	}
 
@@ -301,6 +337,7 @@ int finalizarProcesoMemoria(uint32_t pid,bool force){
 	}
 
 	if(list_find(procesosEXIT->elements,(void*)_condicion) != NULL){
+		pthread_mutex_unlock(&mutex_memoria);
 		return -1; // El proceso ya estaba finalizado
 	}
 	if(list_find(procesosEXEC->elements,(void*)_condicion) != NULL){
@@ -308,23 +345,28 @@ int finalizarProcesoMemoria(uint32_t pid,bool force){
 		if(!force){
 			aux->debeFinalizar = 1;
 			aux->pcb->exitCode = -6;
+			pthread_mutex_unlock(&mutex_memoria);
 			return -2; // No finaliza un proceso hasta que deje de estar en Running
 		}
 	}
 	if(list_find(procesosREADY->elements,(void*)_condicion) != NULL || list_find(procesosNEW->elements,(void*)_condicion) != NULL || list_find(procesosEXEC->elements,(void*)_condicion) != NULL){
 		char* charsito = malloc(1);
 		if(send(idUMC,"F",1,0)<=0){
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		}
 		if(send(idUMC,&pid,sizeof(uint32_t),0) <=0){
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		}
 		if(recv(idUMC,charsito,1,0) <=0){
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		}
 		if(charsito[0] == 'Y'){
 			// puedo continuar, no debo retornar nada aun
 		} else {
+			pthread_mutex_unlock(&mutex_memoria);
 			return -1;
 		}
 	}
@@ -345,6 +387,7 @@ int finalizarProcesoMemoria(uint32_t pid,bool force){
 			moverPrograma(aux,procesosEXEC,procesosEXIT);
 		}
 	}
+	pthread_mutex_unlock(&mutex_memoria);
 	return 0;
 }
 
@@ -703,4 +746,6 @@ void liberarHeapNico(int cpu,t_programa *proximoPrograma){
 /*int liberarPagina(){
 
 }*/
+
+
 
